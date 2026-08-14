3efd7d630e80cc1b49c6d2d85ea54d91dee71fe6
unknown <snapswift.app@gmail.com>
2026-08-14 18:00:46 +0900

    Initial LM Memo project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="page">
+      <section className="card">
+        <h1 className="title">LMメモ</h1>
+        <p className="subtitle">Life Manager Memo</p>
+        <p className="property">東京テラス</p>
+        <p className="status">Web版 開発中</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
